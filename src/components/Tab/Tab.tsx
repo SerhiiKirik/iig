6c8100d212bf9c1ff9c299aaac0a18bdf52a64a6
Sergey Kirichenko <s.kirichenko@m-dev.org>
2023-04-30 21:30:30 +0300

    initial commit

---
+++ src/components/Tab/Tab.tsx
@@ -0,0 +1,26 @@
+import { FC, memo } from 'react';
+import { TabType } from '../../types/TabType';
+
+interface Props {
+  tab: TabType;
+  isActive: boolean;
+  onChange: (tab: TabType) => void;
+}
+
+export const Tab: FC<Props> = memo((props) => {
+  const { tab, isActive, onChange } = props;
+
+  return (
+    <a
+      href={`#${tab.id}`}
+      data-cy="TabLink"
+      onClick={() => {
+        if (!isActive) {
+          onChange(tab);
+        }
+      }}
+    >
+      {tab.title}
+    </a>
+  );
+});
